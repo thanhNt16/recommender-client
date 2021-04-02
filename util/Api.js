c91cc22d9b7dbdf89cb22a9e456d05732fb1acae
thanh <thanh.nt163727@sis.hust.edu.vn>
2021-04-02 10:40:39 +0700

    update ui

--- util/Api.js
+++ util/Api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const httpClient = axios.create({
   baseURL: `https://api.recengine.tech/`, //YOUR_API_URL HERE
+  // baseURL: `http://localhost:8000/`, //YOUR_API_URL HERE
   headers: {
     'Content-Type': 'application/json',
   },
